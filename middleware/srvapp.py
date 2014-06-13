d51cd02ab717a0a7c1bdfbce68b4d6835c1ff574
Valentin <vgulaev@yandex.ru>
2014-06-13 18:18:50 +0600

    Working WSGI

--- middleware/srvapp.py
+++ middleware/srvapp.py
@@ -1,29 +1,22 @@
-from wsgiref.util import setup_testing_defaults
-from wsgiref.simple_server import make_server
-
-# A relatively simple WSGI application. It's going to print out the
-# environment dictionary after being updated by setup_testing_defaults
-class statistic(object):
-	"""docstring for statistic"""
-	calls = 0
-	def __init__(self):
-		self.arg = arg
-		
-def simple_app(environ, start_response):
-    setup_testing_defaults(environ)
-
+def application(environ, start_response):
     status = '200 OK'
+    output = 'Hello World my friend!'
+
+#    response_headers = [('Content-type', 'text/plain'),
+#                        ('Content-Length', str(len(output)))]
+#    start_response(status, response_headers)
     headers = [('Content-type', 'text/plain')]
 
     start_response(status, headers)
 
     ret = ["%s: %s\n" % (key, value)
            for key, value in environ.iteritems()]
-    statistic.calls += 1
-    print "calls: " + str(statistic.calls)
-    #return ret
-    return "Hello word!!!"
+    #statistic.calls += 1
+    #print "calls: " + str(statistic.calls)
+    #return [output]
+    return ret
 
-httpd = make_server('', 8000, simple_app)
-print "Serving on port 8000..."
-httpd.serve_forever()
+    #return [output]
+if __name__ == '__main__':
+    from paste import httpserver
+    httpserver.serve(application, host='127.0.0.1', port='8080')
