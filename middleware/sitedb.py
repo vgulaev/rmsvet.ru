c5317f440a482d36668d7542b66a88ba27ccec82
Valentin <vgulaev@yandex.ru>
2014-08-30 20:35:33 +0600

    Up

--- middleware/sitedb.py
+++ middleware/sitedb.py
@@ -11,10 +11,17 @@ def fl_to_str(v, t):
     if (t.find("decimal") > -1):
         res = "1.33"
     else:
-        res = "'{v}'".format(v = v)
+        res = "'{v}'".format(v = str(v))
     return res
 
-class dbrecord:
+class dbfield:
+    def __init__(self, val = "", t = ""):
+        self.val = val
+    def __str__(self):
+        return self.val
+    __repr__ = __str__
+
+class dbrecord(object):
     def __init__(self):
         __db__ = ""
         __fields__ = []
@@ -26,12 +33,19 @@ class dbrecord:
         keys = ",".join(self.__fields__ )
         values = ",".join(fl_to_str(self[e], t) for (e, t) in zip(self.__fields__, self.__fields_type__))
         sql = sql.format(tn = self.__tablename__, keys = keys, values = values)
-        #sql += " ON DUPLICATE KEY UPDATE {eq}".format(eq = self.fl_for_dup_key())
-        #print sql
-        self.__db__.cursor.execute(sql)
-        self.__db__.db.commit()
+        sql += " ON DUPLICATE KEY UPDATE {eq}".format(eq = self.fl_for_dup_key())
+        print sql
+        #self.__db__.cursor.execute(sql)
+        #self.__db__.db.commit()
     def fl_for_dup_key(self):
-        return ""
+        l = [e + " = " + fl_to_str(self[e], t) for (e, t) in zip(self.__fields__, self.__fields_type__) if e != "id"]
+        return ",".join(l)
+    def __setattr__(self, name, val):
+        if hasattr(self, name):
+            if isinstance(self[name], dbfield):
+                self.__dict__[name] = dbfield(val)
+        else:
+            self.__dict__[name] = dbfield(val)
 
 class dbworker:
     def __init__(self):
@@ -53,15 +67,17 @@ class dbworker:
         self.cursor.execute(sql)
         
         class retclass(dbrecord):
-            pass
+            def __set__(self, obj, val):
+                print "Oh"
         retclass.__db__ = self;
         retclass.__fields__ = []
         retclass.__fields_type__ = []
         retclass.__tablename__ = table_name
+        #retclass.__set__(self, obj, val) = dbrecord.__set__
         row = self.cursor.fetchone()
         while row is not None:
             retclass.__fields__ += [row[0]]
             retclass.__fields_type__ += [row[1]]
-            setattr(retclass, row[0], "")
+            setattr(retclass, row[0], dbfield())
             row =  self.cursor.fetchone()
         return retclass
