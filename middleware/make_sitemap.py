907539fd4e967e3d699f58ac3ca5c43ec5356da1
Valentin <vgulaev@yandex.ru>
2014-12-22 13:25:14 +0500

    Up

---
+++ middleware/make_sitemap.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+import common as cm
+
+import xml.etree.ElementTree as ET
+import datetime
+import sett
+
+import dbclasses.dbworker
+
+dbclasses.dbworker.cred = dbclasses.dbworker.loadmysqlcredential( sett )
+
+dt_now = datetime.datetime.now()
+dt_for_xml = dt_now.strftime( '%Y-%m-%d' )
+
+urlset = ET.Element( "urlset", attrib = { "xmlns" : "http://www.sitemaps.org/schemas/sitemap/0.9" } )
+
+con = dbclasses.dbworker.getcon()
+cursor = con.cursor()
+
+sql = """select fantastic_url from prices"""
+cursor.execute( sql )
+
+lastmod = ET.Element( "lastmod" )
+lastmod.text = dt_for_xml
+priority = ET.Element( "priority" )
+priority.text = "0.8"
+
+k = 0
+row = cursor.fetchone()
+while row is not None:
+    url = ET.Element( "url" )
+    loc = ET.Element( "loc" )
+    loc.text = "http://eazyshop.ru/" + row[ 0 ] + "/goods"
+    
+    url.append( loc )
+    url.append( lastmod )
+    url.append( priority )
+    
+    urlset.append( url )
+    k += 1
+    if k % 100 == 0 :
+        print( k )
+    row = cursor.fetchone()
+    #if k > 3:
+    #    break
+
+
+tree = ET.ElementTree( element = urlset )
+
+tree.write("sitemap.xml", encoding="utf-8", xml_declaration = True)
+
+#print of.child.
+
+print( "End" )
