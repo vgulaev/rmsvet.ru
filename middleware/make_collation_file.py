1d213cb71891e30ac1b683502689869462288d16
Valentin <vgulaev@yandex.ru>
2015-03-08 23:31:00 +0500

    Up

---
+++ middleware/make_collation_file.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+import codecs
+
+eng_sym = [ "a", "c", "e", "o", "x", "m", "k", "y", "p", "t" ]
+rus_sym = [ "а", "с", "е", "o", "х", "м", "к", "у", "р", "т" ]
+
+f = codecs.open( "collation.xml", "w", "utf-8" )
+for ( e, r ) in zip( eng_sym, rus_sym ):
+    f.write( "<reset>{l}</reset>\n".format( l = e ) )
+    f.write( "<i>{l}</i>\n".format( l = r ) )
+    f.write( "<reset>{l}</reset>\n".format( l = e.upper() ) )
+    f.write( "<i>{l}</i>\n".format( l = r.upper() ) )
+    print( e )
+    pass
+print( ord( "х" ) )
+import sys
+print( sys.version )
---
+++ readme.md
@@ -0,0 +1,2 @@
+1. For correct collation have to prepare the add string for "Index.xml" (http://dev.mysql.com/doc/refman/5.7/en/adding-collation.html)
+2. Have to move standart js libs from middleware/libs
