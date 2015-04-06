9c1865d7ba9ef4002b14242d7a0ae61e37aac09b
movis08 <d08121992@mail.ru>
2015-04-06 17:40:06 +0500

    up

--- middleware/checon/CheColl.py
+++ middleware/checon/CheColl.py
@@ -16,19 +16,19 @@ def CheColl(index):
                     flag = '1'
                     a.append(nStr)
             if not bool1:
-                if madeStr == r'<collation name="utf8_general_ci_eng_cy" id="1033"':
+                if madeStr == r'<collation name="utf8_general_ci_eng_cy" id="1033">':
                     flag = '2'
                     a.append(nStr)
+                    break
                 elif madeStr == r'</charset>':
-                    bool1 = True
                     a.append(nStr)
                     break
         if flag == '0':
             print( r'<charset name="utf8"> is not exist' )
         elif flag == '1':
-            print( r'collation name="utf8_general_ci_eng_cy" id="1033" is not exist' )
+            print( r'<collation name="utf8_general_ci_eng_cy" id="1033"> is not exist' )
         elif flag == '2':
-            print( '<charset name="utf8"> and collation name="utf8_general_ci_eng_cy" id="1033" is exist' )
+            print( r'<charset name="utf8"> and <collation name="utf8_general_ci_eng_cy" id="1033"> is exist' )
         if flag == '1':
             middleware = path.normpath(path.join(path.dirname(__file__), r'../'))
             exec(compile(open(middleware + r'/make_collation_file.py', "rb").read(), middleware + r'/make_collation_file.py', 'exec'))
