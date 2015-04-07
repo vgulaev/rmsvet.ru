20f887f90b75f8cf91b09667a733aca9364e042e
movis08 <d08121992@mail.ru>
2015-04-07 11:14:22 +0500

    up

---
+++ middleware/structures/Tree.py
@@ -0,0 +1,48 @@
+class CatalogTree():
+    Child = []
+    CategoryID = ""
+    CategoryName = ""
+    ParentCategoryID = ""
+    ParentLink = ""
+    NestingLevel = 0
+    """
+    Class constructor
+    """
+    def __init__(self, Parent = "", CID = "", CN = "", Level = 0):
+        self.ParentLink = ""
+        self.Child = []
+        self.ParentCategoryID = Parent
+        self.CategoryID = CID
+        self.CategoryName = CN
+        self.NestingLevel = Level
+    """
+    Work with childs
+    """
+    def addChild(self, ParentID = CategoryID, CID = "", CN = ""):
+        child = CatalogTree(ParentID, CID, CN, self.NestingLevel +1)
+        child.ParentLink = self
+        self.Child.append(child)
+    def delChild(self, string = "", variant = 0):
+        for i in range(len (self.Child)):
+            if self.Child[i].CategoryID == string:
+                if variant == 0:
+                    self.Child.clear()
+                elif variant == 1:
+                    self.Child[i].__del__()
+                    break
+                elif variant == 2:
+                    for j in range(len(self.Child[i].Child)):
+                        self.Child[i].Child[j].ParentLink = self
+                        self.Child[i].Child[j].NestingLevel -= 1
+                    self.Child += self.Child[i].Child
+                    self.Child.pop(i)
+                    break
+
+a = CatalogTree()
+a.addChild(CID="1", CN = "asd")
+a.addChild(CID="2", CN = "asf")
+a.addChild(CID="3", CN = "asg")
+b=a.Child[2]
+b.addChild(CID="5", CN="qwe")
+a.delChild("3",2)
+b=3+3
